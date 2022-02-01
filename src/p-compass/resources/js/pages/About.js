da810072c741be249e50e9f471c3d444a4c18890
guppy0123 <dfalcon0001@gmail.com>
2022-02-01 18:08:11 +0900

    router設定

---
+++ src/p-compass/resources/js/pages/About.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core'; //Buttonをインポート
+
+function About() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">React導入できたわな</div>
+                        <h1 class="text-3xl text-red-500">Hello Tailwind</h1>
+                        <div className="card-body">pages下に移動出来た？？</div>
+                        <Button color="secondary" variant="contained" href={`/`}>Homeに遷移ボタン</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
--- src/p-compass/resources/js/route.js
+++ src/p-compass/resources/js/route.js
@@ -5,13 +5,24 @@ import {
     Route,
     Switch,
   } from 'react-router-dom';
-import Example from './pages/Example';
+
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Setting from './pages/Setting';
 
   function App() {
     return (
         <div>
             <Switch>
-                <Route path='/example' exact component={Example} />
+                <Route path='/about' exact component={About} />
+                <Route path='/dashboard' exact component={Dashboard} />
+                <Route path='/' exact component={Home} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/register' exact component={Register} />
+                <Route path='/setting' exact component={Setting} />
             </Switch>
         </div>
     );
