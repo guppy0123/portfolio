ad4e45dd8ded5031dd3705c584ee9b18ea3aed6c
guppy0123 <dfalcon0001@gmail.com>
2022-02-02 00:13:51 +0900

    tailwindCSS

--- src/p-compass/tailwind.config.js
+++ src/p-compass/tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}","./resources/js"
+      ],
     purge: [
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.jsx",
     ],
+    theme: {
+        extend: {
+          colors:{
+            primary: '#74C13A',
+            secondary:{
+              100: '#E2E2D5',
+              200: '#888883',
+            }
+          },
+          fontFamily:{
+            body:['Nunito']
+          }
+        },
+      },
   };
